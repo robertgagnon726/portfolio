4d3208df29a30a4bf64b99818ddd1c84f4ff48e0
Bobby Gagnon <robertgagnon726@gmail.com>
2025-03-04 13:53:37 -0500

    removed logger validations and TODOs

--- src/connected-components/Layout/Layout.tsx
+++ src/connected-components/Layout/Layout.tsx
@@ -4,26 +4,13 @@ import AppBar from '@mui/material/AppBar';
 import { styled } from '@mui/material/styles';
 import { AlertProvider } from '@Components/Alert';
 import { Header } from '@Connected-components/Layout/Header';
-import { PropsWithChildren, useEffect } from 'react';
+import { PropsWithChildren } from 'react';
 import { Box } from '@mui/material';
-import { Logger } from '../../observability/Logger';
 
 /**
  * Layout component renders the main layout of the application to be used across multiple routes in the routing layer.
  */
 export function Layout({ children }: PropsWithChildren) {
-  // TODO BG - remove these validations when done
-  useEffect(() => {
-    Logger.info('Layout rendered');
-    Logger.debug('Layout rendered with children', { children });
-    Logger.warn('Layout rendered with children', { children });
-    try {
-      throw new Error('Layout error');
-    } catch (error) {
-      Logger.error(error, 'Layout error');
-    }
-  }, []);
-
   return (
     <AlertProvider>
       <StyledAppBarContainer>
