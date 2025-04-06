4a42f4729d9a535c94d404bea43ab93533b2d6c4
Bobby Gagnon <robertgagnon726@gmail.com>
2025-04-06 12:39:52 -0400

    Add metadata generation for blog posts including Open Graph and Twitter tags

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { client } from '@Root/sanity/lib/client';
 import { SanityDocument } from 'next-sanity';
 import { GET_BLOG_POST_FOR_BLOG_PAGE } from '@Root/sanity/queries/getBlogPostForBlogPage';
 import { BlogPost } from '@Features/BlogPost/BlogPost';
+import { Metadata } from 'next';
 
 export const revalidate = 30;
 
@@ -17,6 +18,55 @@ interface PostPageProps {
 }
 
 type tParams = Promise<{ slug: string }>;
+
+// Generate metadata for the page including Open Graph tags
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+  // Fetch data for this specific post
+  const post = await getSinglePost(params.slug);
+
+  // If post doesn't exist, return minimal metadata
+  if (!post) {
+    return {
+      title: 'Post Not Found',
+      description: 'This blog post could not be found.',
+    };
+  }
+
+  // Extract the hero image URL from the post data
+  const heroImageUrl = post.mainImage?.asset?.url;
+
+  console.log('heroImageUrl', heroImageUrl);
+
+  // Format the absolute URL for the blog post
+  const postUrl = `https://bobbygagnon.com/blog/${params.slug}`;
+
+  return {
+    title: post.title,
+    description: post.excerpt || post.description || "Read more on Bobby Gagnon's blog",
+    openGraph: {
+      title: post.title,
+      description: post.excerpt || post.description || "Read more on Bobby Gagnon's blog",
+      url: postUrl,
+      siteName: 'Bobby Gagnon',
+      images: [
+        {
+          url: heroImageUrl,
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+      type: 'article',
+      locale: 'en_US',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description: post.excerpt || post.description || "Read more on Bobby Gagnon's blog",
+      images: [heroImageUrl],
+    },
+  };
+}
 
 export default async function PostPage({ params }: PostPageProps) {
   const { slug } = await params;
